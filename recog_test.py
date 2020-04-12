bc2f2f38b7975403268de21000c29d9e8753c664
n0hack <nohack-@naver.com>
2020-04-12 23:16:28 +0900

    first commit

--- recog_test.py
+++ recog_test.py
@@ -1,23 +1,6 @@
-import tensorflow as tf
 import cv2
 
-# -----
-'''
-[nltk_data] Error loading punkt: <urlopen error [SSL:
-[nltk_data]     CERTIFICATE_VERIFY_FAILED] certificate verify failed
-[nltk_data]     (_ssl.c:777)>
-http://blog.pengyifan.com/how-to-fix-python-ssl-certificate_verify_failed/
-'''
-import os
-import ssl
-if (not os.environ.get('PYTHONHTTPSVERIFY', '') and
-    getattr(ssl, '_create_unverified_context', None)):
-    ssl._create_default_https_context = ssl._create_unverified_context
-# ------
-
-file_path = tf.keras.utils.get_file(
-    '24.jpg', 'https://github.com/kairess/age_gender_estimation/raw/master/img/24.jpg')
-image = cv2.imread(file_path)
+image = cv2.imread('sample2.jpg')
 
 eye_cascade = cv2.CascadeClassifier(
     cv2.data.haarcascades + 'haarcascade_eye.xml')
